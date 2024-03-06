8f4e284824e5dc1d0104aa45024e288c9cc60aa5
Maks Kravchenko <123405337+Kravazf@users.noreply.github.com>
2024-03-06 17:21:29 +0200

    start project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div>
+      Hi!
+    </div>
+  )
+};
+
+export default App;
---
+++ src/sections/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import './contact.css';
+
+const Contact = () => {
+  return (
+    <section id='contact'>
+      <h2>Contacts</h2>
+    </section>
+  )
+}
+
+export default Contact;

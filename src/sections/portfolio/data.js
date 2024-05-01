4fbbbba68017b68ce3cd3df4c0ba8b9e20ae1ff6
Maks Kravchenko <123405337+Kravazf@users.noreply.github.com>
2024-05-02 02:27:30 +0300

    add-nake-app

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -1,6 +1,8 @@
 import Ministry from '../../assets/ministry.png';
 import QuizTeskTask from '../../assets/quiz-tesk-task.png';
 import WheatherApp from '../../assets/wheather-app.png';
+import Travel from '../../assets/Travel.png';
+import nike from '../../assets/nike.png';
 
 const data = [
   {
@@ -34,7 +36,21 @@ const data = [
     ],
   },
   {
-    id: 3,
+    id: 4,
+    image: Travel,
+    url: 'https://Kravazf.github.io/travel-app/',
+    title: 'Travel app',
+    technologyStack: [
+      'JS',
+      'React.js',
+      'HTML5',
+      'SCSS',
+      'React-icons',
+      'AOS - Animate on scroll library',
+    ],
+  },
+  {
+    id: 5,
     image: WheatherApp,
     url: 'https://kravazf.github.io/wheather-app/',
     title: 'Wheather App',
@@ -44,6 +60,20 @@ const data = [
       'HTML5',
       'CSS3',
       'Styled-component',
+    ],
+  },
+  {
+    id: 6,
+    image: nike,
+    url: 'https://kravazf.github.io/vite-nike-landing-deploy/',
+    title: 'Nike Clone App',
+    technologyStack: [
+      'JS',
+      'React.js',
+      'HTML5',
+      'Tailwind CSS',
+      'clsx',
+      'Vite.js',
     ],
   },
 ];
